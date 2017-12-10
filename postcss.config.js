b62714d05759f040f101a7a784f91b9160b4c1eb
Dean Glueckler <glueckler@gmail.com>
2017-12-09 23:00:13 -0800

    keep pushing what you do

---
+++ postcss.config.js
@@ -0,0 +1,13 @@
+//npm i -D postcss postcss-load-config postcss-cssnext postcss-import postcss-simple-vars postcss-simple-vars postcss-hexrgba postcss-mixins postcss-nested
+module.exports = {
+  map: false,
+  plugins: {
+    'postcss-import': {},
+    'postcss-simple-vars' : {},
+    'postcss-cssnext': {},
+    'postcss-simple-vars': {},
+    'postcss-hexrgba': {},
+    'postcss-mixins': {},
+    'postcss-nested': {}
+  }
+}
---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import React, { Component } from 'react'
+import './styles/styles.css'
+
+class App extends Component {
+
+  render() {
+    return (
+        <div className="App">
+        <img className="background" src="../public/images/bg.jpeg" alt="ill gates art" />
+        </div>
+      )
+  }
+}
+
+export default App
